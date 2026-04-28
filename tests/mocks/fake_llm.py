3a66659b5474553cd2ad65ffa7b70b80246de157
Dang Hoang Khang <khangrubikcuber@gmail.com>
2026-04-28 20:00:08 +0700

    fix CI

--- tests/mocks/fake_llm.py
+++ tests/mocks/fake_llm.py
@@ -1,5 +1,15 @@
-class FakeLLM:
-    def invoke(self, prompt: str):
+from langchain_core.language_models.llms import LLM
+from typing import Any, List, Optional
+from langchain_core.callbacks.manager import CallbackManagerForLLMRun
+
+class FakeLLM(LLM):
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
         prompt = prompt.lower()
 
         if "csv" in prompt or "data" in prompt:
@@ -8,3 +18,7 @@ class FakeLLM:
             return "ROUTE: RAG"
         else:
             return "ROUTE: GENERAL"
+
+    @property
+    def _llm_type(self) -> str:
+        return "fake"
